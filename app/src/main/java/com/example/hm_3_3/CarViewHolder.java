da0ddab7333966b75b877095292757c9c5d69554
User <askarmustafaev015@gmail.ru>
2023-01-27 19:37:18 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hm_3_3/CarViewHolder.java
@@ -0,0 +1,20 @@
+package com.example.hm_3_3;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CarViewHolder extends RecyclerView.ViewHolder {
+    private final TextView tvCar;
+
+    public CarViewHolder(@NonNull View itemView) {
+        super(itemView);
+        tvCar = itemView.findViewById(R.id.tv_car);
+    }
+
+    public void bind(String car) {
+        tvCar.setText(car);
+    }
+}
